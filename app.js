3feeddb3b60557d8a7c8736e54631a0b52d3d780
robbyxsuganda <robbyxsuganda@gmail.com>
2025-01-07 17:19:35 +0700

    feat: add set up sequlize config

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
-app.use(router);
+// app.use(router);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
