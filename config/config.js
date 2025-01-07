8a3fcfca36529e003b23ac0b618e30c59095e859
robbyxsuganda <robbyxsuganda@gmail.com>
2025-01-07 17:20:52 +0700

    change dialect

--- config/config.js
+++ config/config.js
@@ -5,20 +5,20 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": "mysql"
+    "dialect": "postgres"
 },
 "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
 },
 "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
 }
 };
