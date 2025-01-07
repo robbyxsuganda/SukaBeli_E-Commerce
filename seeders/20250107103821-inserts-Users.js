79ca161beba4e6c934a7c8df9fdd83b976fc6131
robbyxsuganda <robbyxsuganda@gmail.com>
2025-01-07 17:58:07 +0700

    feat: seed users

---
+++ seeders/20250107103821-inserts-Users.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const fs = require("fs").promises;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const usersJson = JSON.parse(await fs.readFile("./data/users.json", "utf-8")).map((user) => {
+      delete user.id;
+      user.createdAt = user.updatedAt = new Date();
+      return user;
+    });
+
+    await queryInterface.bulkInsert("Users", usersJson);
+
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null);
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
