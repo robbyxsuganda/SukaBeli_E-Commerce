4771a586b607d28d10ac981206a6d51984474cc5
robbyxsuganda <robbyxsuganda@gmail.com>
2025-01-07 15:15:41 +0700

    nambhkan folder

---
+++ routers/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+// const storeRoute = require("./storeRoute");
+// const employeeRoute = require("./employeeRoute");
+
+// router.use(storeRoute);
+// router.use(employeeRoute);
+
+module.exports = router;
